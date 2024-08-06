59d7f1857c7e04abd7afc0c832e425a8cc412d34
kimu7466 <kimu7466@gmail.com>
2024-08-06 13:39:24 +0530

    optimized code

--- edabit_question/03_Medium/03_04-08-2024/task_38_generation.js
+++ edabit_question/03_Medium/03_04-08-2024/task_38_generation.js
@@ -25,32 +25,22 @@ Generation	Male	Female
 2	grandson	granddaughter
 3	great grandson	great granddaughter
 Successfully added to collection.
- */
+*/
 
 function generation(idx, gender){
-    let generation_array = [["me!(boy)","me!(girl)"], ["son", "daughter"], ["grandson", "granddaughter"], ["great grandson", "great granddaughter"], ["great grandfather", "great grandmother"], ["grandfather", "grandmother"], ["father", "mother"]]
-    let res_generation = [];
-    if ( parseInt(idx) >=0 ){    
-        res_generation = generation_array[idx]
-    }
-    else if(parseInt(idx)<0){
-        res_generation = generation_array[generation_array.length + idx]
-    }
-    let g = 0;
-    if ( gender === "f" ) {
-        g = 1;
-    }
-    let result = res_generation[g];
+    let result = (idx != 0 && gender == "m") ? "son" : ( idx != 0 && gender == "f") ?  "daughter" : "me!";
+    let obj =   { 2:"grand ", 3:"great grand "};
+    if (Math.abs(idx)>1){result = obj[Math.abs(idx)] + result};
     return result;
 }
 
 
-console.log(generation(3, "m"));    // great grandson
-console.log(generation(3, "f"));    // great granddaughter
+console.log(generation(3, "m"));    // great grand son
+console.log(generation(3, "f"));    // great grand daughter
 
 
-console.log(generation(2, "m"));    // grandson
-console.log(generation(2, "f"));    // granddaughter
+console.log(generation(2, "m"));    // grand son
+console.log(generation(2, "f"));    // grand daughter
 
 
 console.log(generation(1, "m"));    // son
@@ -65,12 +55,64 @@ console.log(generation(-1, "m"));    //father
 console.log(generation(-1, "f"));    //mother
 
 
-console.log(generation(-2, "m"));   // grandfather
-console.log(generation(-2, "f"));   // grandmother
+console.log(generation(-2, "m"));   // grand father
+console.log(generation(-2, "f"));   // grand mother
 
 
-console.log(generation(-3, "m"));   // great grandfather
-console.log(generation(-3, "f"));   // great grandmother"
+console.log(generation(-3, "m"));   // great grand father
+console.log(generation(-3, "f"));   // great grand mother"
 
+
+
+
+
+
+
+
+
+// function generation(idx, gender){
+//     let generation_array = [["me!(boy)","me!(girl)"], ["son", "daughter"], ["grandson", "granddaughter"], ["great grandson", "great granddaughter"], ["great grandfather", "great grandmother"], ["grandfather", "grandmother"], ["father", "mother"]]    
+//     let res_generation = [];
+//     if ( parseInt(idx) >=0 ){    
+//         res_generation = generation_array[idx]    
+//     }
+//     else if(parseInt(idx)<0){
+//         res_generation = generation_array[generation_array.length + idx]    
+//     }
+//     let g = 0;
+//     if ( gender === "f" ) {
+//         g = 1;    
+//     }
+//     let result = res_generation[g];
+//     return result;
+// }
+
+
+// console.log(generation(3, "m"));    // great grandson
+// console.log(generation(3, "f"));    // great granddaughter
+
+
+// console.log(generation(2, "m"));    // grandson
+// console.log(generation(2, "f"));    // granddaughter
+
+
+// console.log(generation(1, "m"));    // son
+// console.log(generation(1, "f"));    // daughter
+
+
+// console.log(generation(0, "m"));    // me!
+// console.log(generation(0, "f"));    // me!
+
+
+// console.log(generation(-1, "m"));    //father
+// console.log(generation(-1, "f"));    //mother
+
+
+// console.log(generation(-2, "m"));   // grandfather
+// console.log(generation(-2, "f"));   // grandmother
+
+
+// console.log(generation(-3, "m"));   // great grandfather
+// console.log(generation(-3, "f"));   // great grandmother"
 
 
