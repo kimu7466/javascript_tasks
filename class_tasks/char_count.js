3f7a4c99c011ca2ff35c5f3d9f9d2db142795269
kimu7466 <kimu7466@gmail.com>
2024-08-16 23:27:00 +0530

    #

---
+++ class_tasks/char_count.js
@@ -0,0 +1,57 @@
+let string_ = "my name is imroz khan.";
+
+// function charCount(string){
+//     count_arr = []
+//     arr = string.split("")
+
+//     arr.forEach(element => {
+
+//         count = 1;
+//         sub_arr = [element, count]
+
+//         if (count_arr.includes(element)){
+//             // count_arr.push([sub_arr[0], sub_arr[1]+1])
+//             sub_arr[1] += 1
+//         }
+
+//         else{
+//             count_arr.push(sub_arr)
+//         }
+
+//     });
+
+//     console.log(count_arr);
+
+// }
+
+// charCount(string_)
+
+
+
+
+
+// sudo code to do with object
+
+// function charCount(string){
+// count_obj = {}
+// string_.split("")
+// string_.forEach(element => {
+//     if (count_obj.includes(element)){
+//         Object.add(element, count = 1)
+//     }else{
+//         Object.assign(count_obj, element : count += 1)
+//     }
+// });
+// }
+// charCount(string_)
+
+
+arr = ["a", "a", 'a']
+
+obj = []
+
+obj.push(arr[0]) 
+
+obj.a += 1
+
+console.log(obj);
