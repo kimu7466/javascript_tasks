838b469987459e71a2a7743a769233dd41c3a4cd
kimu7466 <kimu7466@gmail.com>
2024-08-26 17:44:02 +0530

    new task word count using object

---
+++ class_tasks/word_count.js
@@ -0,0 +1,17 @@
+string_ = "my name is imroz khan ... and i am a full stack developer ... full stack developer ... imroz khan"
+
+function word_count(string) {
+    let obj =  {}
+    let str_arr = string.split(" ")
+    for (let word of str_arr){
+        if (obj[word]){
+            obj[word] += 1
+        }
+        else{
+            obj[word] = 1
+        }
+    }
+    return obj
+}
+
+console.log(word_count(string_));
