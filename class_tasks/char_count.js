22f50ca06dea30bb9241aa1d523dbf06a6c2df22
kimu7466 <kimu7466@gmail.com>
2024-08-26 16:06:25 +0530

    correction

--- class_tasks/char_count.js
+++ class_tasks/char_count.js
@@ -1,57 +1,18 @@
 let string_ = "my name is imroz khan.";
 
-// function charCount(string){
-//     count_arr = []
-//     arr = string.split("")
 
-//     arr.forEach(element => {
+function charCount(string){
+    let obj = {}
+    for(let char of string){
+        if (obj[char]){
+            obj[char] += 1;
+        }
+        else{
+            obj[char] = 1;
+        }
+    };
+    return obj;
+}
 
-//         count = 1;
-//         sub_arr = [element, count]
+console.log(charCount(string_));
 
-//         if (count_arr.includes(element)){
-//             // count_arr.push([sub_arr[0], sub_arr[1]+1])
-//             sub_arr[1] += 1
-//         }
-
-//         else{
-//             count_arr.push(sub_arr)
-//         }
-
-//     });
-
-//     console.log(count_arr);
-
-// }
-
-// charCount(string_)
-
-
-
-
-
-// sudo code to do with object
-
-// function charCount(string){
-// count_obj = {}
-// string_.split("")
-// string_.forEach(element => {
-//     if (count_obj.includes(element)){
-//         Object.add(element, count = 1)
-//     }else{
-//         Object.assign(count_obj, element : count += 1)
-//     }
-// });
-// }
-// charCount(string_)
-
-
-arr = ["a", "a", 'a']
-
-obj = []
-
-obj.push(arr[0]) 
-
-obj.a += 1
-
-console.log(obj);
